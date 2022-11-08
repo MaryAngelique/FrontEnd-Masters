23e2092e36aacf79f5bd9a65814408f9ae338d09
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-08 10:43:30 -0500

    Implemented typeof number, string, boolean, undefined, object, null object, and array object

---
+++ Getting Started With JS/Types.js
@@ -0,0 +1,20 @@
+typeof 4;
+// Number
+
+typeof "Angelique";
+// String
+
+typeof true;
+// Boolean
+
+typeof undefined;
+// undefined
+
+typeof { age: 21 };
+// Object
+
+typeof null;
+// Object !?!?
+
+typeof [1,2,3];
+// Object
