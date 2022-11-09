b692368efb1f1b45106b3f4258b9b74a7b89207e
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-09 11:31:18 -0500

    Implemented Boolean with equality

---
+++ Getting Started With JS/Boolean.js
@@ -0,0 +1,10 @@
+var workshopEnrollment1 = 16;
+var workshopEnrollment2 = worksjop2Elem.value;
+
+if (Number(workshopEnrollment1) < Number(workshopEnrollment2)) {
+    //..
+}
+
+if (workshopEnrollment1 < workshopEnrollment2) {
+    //..
+}
